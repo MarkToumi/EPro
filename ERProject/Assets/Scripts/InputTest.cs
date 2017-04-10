52deb47ea21cb02a57b29918f3a04aa19cf24064
GIGABYTE\dijsf_000 <psp2ipso2moka@gmail.com>
2017-04-11 00:09:42 +0900

    入力検知テスト用

---
+++ ERProject/Assets/Scripts/InputTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("GamePad01_Fire")) Debug.Log("テスト射撃成功");
+        if (Input.GetButtonDown("GamePad01_Jump")) Debug.Log("テスト跳躍成功");
+        if (Input.GetAxis("GamePad01_X") >= 0.1) Debug.Log("横移動@Axis:" + Input.GetAxis("GamePad01_X") + ",AxisRaw:" + Input.GetAxisRaw("GamePad01_X"));
+        if (Input.GetAxis("GamePad01_Y") >= 0.1) Debug.Log("縦移動@Axis:" + Input.GetAxis("GamePad01_Y") + ",AxisRaw:" + Input.GetAxisRaw("GamePad01_Y"));
+    }
+}
