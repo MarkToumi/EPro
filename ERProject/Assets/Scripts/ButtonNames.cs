f38982fbbe33a68f00cf76c393af3d930adecc23
GIGABYTE\dijsf_000 <psp2ipso2moka@gmail.com>
2017-04-11 01:26:10 +0900

    Input系を改悪（）

---
+++ ERProject/Assets/Scripts/ButtonNames.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ButtonName
+{
+    public class GamePad01 : MonoBehaviour
+    {
+        private static string horizontal = "GamePad01_X";
+        private static string vertical = "GamePad01_Y";
+        private static string fire = "GamePad01_Fire";
+        private static string jump = "GamePad01_Jump";
+        public static float X() { return Input.GetAxis(horizontal); }
+        public static float Y() { return Input.GetAxis(vertical); }
+        public static bool Fire() { return Input.GetButtonDown(fire); }
+        public static bool Jump() { return Input.GetButtonDown(jump); }
+    }
+
+    public class GamePad02 : MonoBehaviour
+    {
+        private static string horizontal = "GamePad02_X";
+        private static string vertical = "GamePad02_Y";
+        private static string fire = "GamePad02_Fire";
+        private static string jump = "GamePad02_Jump";
+        public static float X() { return Input.GetAxis(horizontal); }
+        public static float Y() { return Input.GetAxis(vertical); }
+        public static bool Fire() { return Input.GetButtonDown(fire); }
+        public static bool Jump() { return Input.GetButtonDown(jump); }
+    }
+
+    public class GamePad03 : MonoBehaviour
+    {
+        private static string horizontal = "GamePad03_X";
+        private static string vertical = "GamePad03_Y";
+        private static string fire = "GamePad03_Fire";
+        private static string jump = "GamePad03_Jump";
+        public static float X() { return Input.GetAxis(horizontal); }
+        public static float Y() { return Input.GetAxis(vertical); }
+        public static bool Fire() { return Input.GetButtonDown(fire); }
+        public static bool Jump() { return Input.GetButtonDown(jump); }
+    }
+
+    public class GamePad04 : MonoBehaviour
+    {
+        private static string horizontal = "GamePad04_X";
+        private static string vertical = "GamePad04_Y";
+        private static string fire = "GamePad04_Fire";
+        private static string jump = "GamePad04_Jump";
+        public static float X() { return Input.GetAxis(horizontal); }
+        public static float Y() { return Input.GetAxis(vertical); }
+        public static bool Fire() { return Input.GetButtonDown(fire); }
+        public static bool Jump() { return Input.GetButtonDown(jump); }
+    }
+
+    public class KeyBord : MonoBehaviour
+    {
+        private static string horizontal = "Key_X";
+        private static string vertical = "Key_Y";
+        private static string fire = "Key_Fire";
+        private static string jump = "Key_Jump";
+        public static float X() { return Input.GetAxis(horizontal); }
+        public static float Y() { return Input.GetAxis(vertical); }
+        public static bool Fire() { return Input.GetButtonDown(fire); }
+        public static bool Jump() { return Input.GetButtonDown(jump); }
+    }
+}
